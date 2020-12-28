15c8bce180869dade299b40403469f8ec46c6066
Amanda <amanda.marry@hotmail.com>
2020-12-28 19:18:49 -0300

    adicionado certificado da profa Debora

---
+++ certificados/script.sql
@@ -0,0 +1,6 @@
+﻿SELECT l.nome, l.cpf, i.email, COUNT(l.cpf)  AS qtd
+	FROM listapresenca AS l
+	INNER JOIN inscricao AS i ON l.cpf = i.cpf
+	WHERE l.cpf IN (SELECT cpf FROM inscricao)	
+	GROUP BY l.cpf, l.nome, i.email
+	HAVING COUNT(l.cpf) >= 10
